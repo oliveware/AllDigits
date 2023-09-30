93bf3bbb5f599160cacce8e0f656f18849da90e8
herve <crespel.herve@wanadoo.fr>
2023-09-30 03:49:35 +0200

    décomposition en groupes de chiffres séparation Pad et Enchiffres

--- Sources/AllDigits/interface/Pad.swift
+++ Sources/AllDigits/interface/Pad.swift
@@ -1,45 +1,7 @@
 import SwiftUI
 import Digiconf
 
-// Interface du package
 
-// nombre écrit avec les chiffres de la numération
-public struct Enchiffres: View {
-    var numeration : Numeration
-    var chiffres: Chiffres
-    var config: Digiconfig
-    var graphic = false
-    
-    public init(_ conf:Digiconfig, _ numeration: Numeration = Numeration(.global, 10),_ chiffres:Chiffres) {
-        self.numeration = numeration
-        graphic = numeration.graphism != nil
-        self.chiffres = chiffres
-        config = conf
-    }
-    
-    public var body: some View {
-        VStack (alignment:.trailing) {
-            HStack {
-                ForEach(0..<chiffres.values.count, id:\.self) {
-                    power in
-                    if graphic {
-                        Chiffregraphic(
-                            index:chiffres.values[power],
-                            graphism:numeration.graphism!,
-                            config: config
-                        )
-                    } else {
-                        Chiffreunicode(
-                            symbol:numeration.chiffre(chiffres.values.count - 1 , power, chiffres.values[power]),
-                            config: config
-                        )
-                    }
-                }
-            }.frame(height: config.haut)
-            Text("\(chiffres.global10)")
-        }
-    }
-}
 
 // nota: la présentation en boules est fournie par Abacus
     
