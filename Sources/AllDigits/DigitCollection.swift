93e2be65ae109c0bb634e260693a3fd9541ef2df
herve <crespel.herve@wanadoo.fr>
2023-09-30 23:53:13 +0200

    adaptation collection

--- Sources/AllDigits/DigitCollection.swift
+++ Sources/AllDigits/DigitCollection.swift
@@ -16,17 +16,19 @@ public struct DigitCollection: View {
     var height:CGFloat = 800
     var cote:CGFloat = 40
     var config = Digiconfig(50,50,0,.brown,.clear)
+    var greatest: Int = 59
     
-    @State var value = 7
+    @State var value = 59
     @State var start = false
     
     var antik: [Numeration] { Numeration.set(.antik, [.global]) }
     var live: [Numeration] { Numeration.set(.live, [.global]) }
 
-    public init(w:CGFloat = 1200, h:CGFloat = 800) {
+    public init(w:CGFloat = 1200, h:CGFloat = 800, maxi:Int = 59) {
         width = w
         height = h
         start = true
+        greatest = maxi
     }
     public var body: some View {
 
@@ -45,18 +47,18 @@ public struct DigitCollection: View {
                     Text("\(value)")
                         .font(config.scale(3).font)
                         .fontWeight(Font.Weight.bold)
-                        .offset(x:-10, y:10)
+                        .offset(x:0, y:10)
                     
                 }.frame(width:width, height:height, alignment: .center)
                     .foregroundColor(Color.green)
-                ForEach (crown([190], antik.count), id: \.self) { point in
+                ForEach (crown([210], antik.count), id: \.self) { point in
                     VStack {
                         Enchiffres(config, Chiffres(value, antik[point.i].base), antik[point.i], false)
                         Text(antik[point.i].numicode.rawValue).font(.caption)
                     }
                     .offset(x: point.x, y: point.y)
                 }
-                ForEach (crown([300], live.count), id: \.self) { point in
+                ForEach (crown([340], live.count), id: \.self) { point in
                     VStack {
                         Enchiffres(config, Chiffres(value, live[point.i].base), live[point.i], false)
                         Text(live[point.i].numicode.rawValue).font(.caption)
@@ -65,19 +67,19 @@ public struct DigitCollection: View {
                 }
                 
                 HStack {
-                    if value > 0 {
-                        Button(action: dec)
-                        {Image(systemName: "chevron.left").scaledToFill()}
-                            .padding(.leading, 15)
-                            .fond(w: cote, h: cote)
-                    }
+                    
+                    Button(action: dec)
+                    {Image(systemName: "chevron.left").scaledToFill()}
+                        .padding(.leading, 15)
+                        .fond(w: cote, h: cote)
+                   
                     Spacer()
-                    if value < 99 {
-                        Button(action: inc)
-                        { Image(systemName: "chevron.right").scaledToFill() }
-                            .padding(.trailing, 15)
-                            .fond(w: cote, h: cote)
-                    }
+                    
+                    Button(action: inc)
+                    { Image(systemName: "chevron.right").scaledToFill() }
+                        .padding(.trailing, 15)
+                        .fond(w: cote, h: cote)
+                    
                 }
                 .padding(10)
             }
@@ -93,14 +95,14 @@ public struct DigitCollection: View {
     
     func dec() {
         if value == 0 {
-            value = 99
+            value = greatest
         } else {
             value -= 1
         }
     }
     
     func inc() {
-        if value == 99 {
+        if value == greatest {
             value = 0
         } else {
             value += 1
@@ -116,7 +118,7 @@ public struct DigitCollection: View {
     func crown(_ rayons:[Int], _ nbp:Int)-> [Point] {
         let pointsparcercle = Int(Double(nbp) / Double(rayons.count))
         let arc = 2 * 3.14 / Double(pointsparcercle)
-        let origin = Point (x:-10, y: height/2, i:0)
+        let origin = Point (x:15, y: height/2-25, i:0)
         var result : [Point] = []
         var cercle = 0
         for rayon in rayons {
